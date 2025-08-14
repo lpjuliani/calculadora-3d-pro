0686db416ec1749bcf72c6a8f8e6b4f5a99bbbf3
lpjuliani <lpjuliani@hotmail.com>
2025-08-14 18:47:59 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
-import { AppProvider } from './context/AppContext'; // se você já usa o AppProvider globalmente
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <AppProvider>
-        <App />
-      </AppProvider>
+      <App />
     </AuthProvider>
   </StrictMode>
 );
